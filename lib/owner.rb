b9092a880396649ae73c13129705fca2a67fc1eb
Ryan Pethel <arpethel@gmail.com>
2019-04-13 16:30:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -76,6 +76,8 @@ class Owner
 
   def list_pets
     # binding.pry
+    fish = @pets[:fishes].count
+    binding.pry
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cat].count} cat(s)."
   end
 
