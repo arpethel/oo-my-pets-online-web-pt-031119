7fe709ced8a9c2ef7d521ae7613e1f9c90b08b8f
Ryan Pethel <arpethel@gmail.com>
2019-04-13 15:03:57 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -3,12 +3,9 @@ class Fish
   attr_accessor :mood
   attr_reader :name
 
-  binding.pry
-
   def initialize(name)
     @name = name
     @mood = "nervous"
-    # binding.pry
   end
 
   def fish_name=(name)
