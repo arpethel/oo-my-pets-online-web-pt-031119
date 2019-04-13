c22d5481198c6f3929a0e467e8a1e3a4b8a4575b
Ryan Pethel <arpethel@gmail.com>
2019-04-13 14:10:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,7 +2,16 @@ require 'pry'
 class Owner
   attr_accessor :pets, :name
 
+  @@all = []
+  @@count = 0
+
   def initialize(name)
     @name = name
+    @@count + 1
   end
+
+  def self.count
+    @@count
+  end
+  
 end
